06425e73c835162afe39bcc5b3b89246a82c6f09
shubhamJoshi4u <shubham3388@gmail.com>
2019-09-21 15:47:31 +0530

    updating the project

---
+++ oddjobs/src/main/java/com/odd/jobs/oddjobs/OddjobsApplication.java
@@ -0,0 +1,13 @@
+package com.odd.jobs.oddjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddjobsApplication.class, args);
+	}
+
+}
